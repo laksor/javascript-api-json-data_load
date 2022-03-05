f8d41ad62b6ff3cbbb508eb862b0e4362a19a82d
laskor <al.moksud@gmail.com>
2022-03-05 02:19:45 -0500

    Dynamically display loaded data

--- js/api.js
+++ js/api.js
@@ -1,13 +1,30 @@
 function loadData(){
   fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(res => res.json())
-  .then(data => console.log(data))
+    .then(res => res.json())
+    .then(data => console.log(data));
   
 }
 
 function loadUser(){
   fetch('https://jsonplaceholder.typicode.com/users')
-  .then(res => res.json())
-  .then(data => console.log(data))
+    .then(res => res.json())
+    .then(data => getUser(data));
   
 }
+function getUser(data){
+  const ul = document.getElementById('user');
+  for(const user of data){
+    const li = document.createElement('li');
+    li.innerText = `name: ${user.name} ...... email: ${user.email}`;
+    ul.appendChild(li);
+
+  }
+}
+
+function loadPost(){
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => console.log(data));
+  
+}
+
