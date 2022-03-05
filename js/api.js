5fb7a3070089859ee79963255f26513091a0ddf6
laskor <al.moksud@gmail.com>
2022-03-05 01:08:41 -0500

    user added

--- js/api.js
+++ js/api.js
@@ -4,3 +4,10 @@ function loadData(){
   .then(data => console.log(data))
   
 }
+
+function loadUser(){
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
+  .then(data => console.log(data))
+  
+}
