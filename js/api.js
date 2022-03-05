efb056371745ec3a871d6a2fadc17825c4af224f
laskor <al.moksud@gmail.com>
2022-03-05 03:06:15 -0500

    display post

--- js/api.js
+++ js/api.js
@@ -15,7 +15,7 @@ function getUser(data){
   const ul = document.getElementById('user');
   for(const user of data){
     const li = document.createElement('li');
-    li.innerText = `name: ${user.name} ...... email: ${user.email}`;
+    li.innerText = `name: ${user.name} email: ${user.email}`;
     ul.appendChild(li);
 
   }
@@ -24,7 +24,16 @@ function getUser(data){
 function loadPost(){
   fetch('https://jsonplaceholder.typicode.com/posts')
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => getPost(data));
   
 }
 
+function getPost(data){
+  const u = document.getElementById('post');
+  for(const post of data){
+    const l = document.createElement('li');
+    l.innerText = `title: ${post.title} .....
+     body: ${post.body}`;
+    u.appendChild(l);
+  }
+}
